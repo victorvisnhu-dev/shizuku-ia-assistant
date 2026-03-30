0815bb4aed2e4362513e53d5ed3013c4f3b7f503
victorvisnhu-dev <victorvisnhu@gmail.com>
2026-03-30 10:45:12 -0600

    Create IAConnector.kt file for connecting to local Ollama AI

---
+++ app/src/main/kotlin/com/victorvishnudev/shizukuia/services/IAConnector.kt
@@ -0,0 +1,10 @@
+// IAConnector.kt
+
+package com.victorvishnudev.shizukuia.services
+
+class IAConnector {
+    // Implement functions to connect to the local Ollama AI
+    fun connect() {
+        // Connection logic
+    }
+}
